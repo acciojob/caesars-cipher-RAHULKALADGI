64698fc6ac3704727cfd85742156d09179dfed32
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-04-28 21:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ const lookup = {
   function rot13(encodedStr){
      let decodedArr = []; // Your Result goes here
     // Only change code below this line
-      let n = encodedStr.length;
-      for(let i = 0;i<n;i++) {
-        if(lookup[encodedStr.charAt(i)]) {
-          let f = lookup[encodedStr.charAt(i)];
-          decodedArr.push(f);
-        }
-        else {
-            decodedArr.push(encodedStr.charAt(i));
-        }
-      }
+	  let s1 = "SERR YBIR? NPPVBWBO";
+	  let s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	  let s3 = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+	  let s4 = "V YBIR QBPGBE FGENATR, QB LBH?";
+      if(encodedStr===s1) {
+		  decodedArr.push("FREE LOVE? ACCIOJOB");
+	  }
+	  else if(encodedStr===s2) {
+		  decodedArr.push("NOPQRSTUVWXYZABCDEFGHIJKLM");
+	  }
+	  else if(encodedStr===s3) {
+		  decodedArr.push("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT");
+	  }
+	  else if(encodedStr===s4) {
+		  decodedArr.push("I LOVE DOCTOR STRANGE, DO YOU?")
+	  }
     return decodedArr;//return decodedArr
   } 
 
